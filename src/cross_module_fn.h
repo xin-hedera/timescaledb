71a5dc060c5d59b13fb2afec4ff1064ea7e7a2ab
Erik Nordström <erik@timescale.com>
2018-12-14 10:57:55 +0100

    Add mappings table for remote hypertables
    In a multi-node (clustering) setup, TimescaleDB needs to track which
    remote servers have data for a particular distributed hypertable. It
    also needs to know which servers to place new chunks on and to use in
    queries against a distributed hypertable.
    
    A new metadata table, `hypertable_server` is added to map a local
    hypertable ID to a hypertable ID on a remote server. We require that
    the remote hypertable has the same schema and name as the local
    hypertable.
    
    When a local server is removed (using `DROP SERVER` or our
    `delete_server()`), all remote hypertable mappings for that server
    should also be removed.

--- src/cross_module_fn.h
+++ src/cross_module_fn.h
@@ -12,6 +12,7 @@
 #include <optimizer/planner.h>
 #include <utils/timestamp.h>
 #include <utils/jsonb.h>
+#include <utils/array.h>
 
 #include "export.h"
 #include "bgw/job.h"
@@ -26,6 +27,9 @@
  * This will allow the function to be called from this codebase as
  *     ts_cm_functions-><function name>
  */
+
+typedef struct JsonbParseState JsonbParseState;
+typedef struct Hypertable Hypertable;
 
 typedef struct CrossModuleFunctions
 {
@@ -70,6 +74,8 @@ typedef struct CrossModuleFunctions
 	Datum (*delete_server)(PG_FUNCTION_ARGS);
 	Datum (*create_chunk)(PG_FUNCTION_ARGS);
 	Datum (*show_chunk)(PG_FUNCTION_ARGS);
+	List *(*get_servername_list)(void);
+	void (*hypertable_make_distributed)(Hypertable *ht, ArrayType *servers);
 	Datum (*timescaledb_fdw_handler)(PG_FUNCTION_ARGS);
 	Datum (*timescaledb_fdw_validator)(PG_FUNCTION_ARGS);
 } CrossModuleFunctions;

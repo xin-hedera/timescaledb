3e074413fa411aecbf6da3facdc3f85c5bdbd44c
Erik Nordström <erik@timescale.com>
2019-01-25 18:49:49 +0100

    Create chunks on remote servers
    This change ensures that chunk replicas are created on remote
    (datanode) servers whenever a chunk is created in a local distributed
    hypertable.
    
    Remote chunks are created using the `create_chunk()` function, which
    has been slightly refactored to allow specifying an explicit chunk
    table name. The one making the remote call also records the resulting
    remote chunk IDs in its `chunk_server` mappings table.
    
    Since remote command invokation without super-user permissions
    requires password authentication, the test configuration files have
    been updated to require password authentication for a cluster test
    user that is used in tests.

--- src/chunk.h
+++ src/chunk.h
@@ -123,8 +123,9 @@ extern TSDLLEXPORT List *ts_chunk_do_drop_chunks(Oid table_relid, Datum older_th
 												 bool cascades_to_materializations,
 												 int32 log_level);
 extern TSDLLEXPORT Chunk *ts_chunk_find_or_create_without_cuts(Hypertable *ht, Hypercube *hc,
-															   const char *schema,
-															   const char *prefix, bool *created);
+															   const char *schema_name,
+															   const char *table_name,
+															   bool *created);
 extern List *ts_chunk_servers_copy(Chunk *chunk);
 
 #define chunk_get_by_name(schema_name, table_name, num_constraints, fail_if_not_found)             \

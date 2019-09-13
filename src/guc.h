c6e7db6c5305bccae6e6552ae2573ae1bdcf6440
niksa <niksa@timescale.com>
2019-09-13 10:13:21 +0200

    Add GUC for AsyncAppend
    GUC timescaledb.enable_async_append enables users to turn on/off async
    append optimization. It is on by default.

--- src/guc.h
+++ src/guc.h
@@ -20,6 +20,7 @@ extern bool ts_guc_enable_parallel_chunk_append;
 extern bool ts_guc_enable_runtime_exclusion;
 extern bool ts_guc_enable_constraint_exclusion;
 extern TSDLLEXPORT bool ts_guc_enable_per_data_node_queries;
+extern TSDLLEXPORT bool ts_guc_enable_async_append;
 extern bool ts_guc_restoring;
 extern int ts_guc_max_open_chunks_per_insert;
 extern int ts_guc_max_cached_chunks_per_hypertable;

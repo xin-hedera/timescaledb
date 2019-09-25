ae8e032a16919e43d1fca8d8c61359ad87e27871
Mats Kindahl <mats@timescale.com>
2019-09-25 10:15:51 +0200

    Add debug printout for optimizer
    Debug printouts are added at two locations:
    
    * Inside `get_foreign_upper_paths` a notice with the existing paths is
      send back together with the stage if the `show_upper` is set to show
      the stage.
    
    * Inside `set_rel_pathlist` a notice is printed with the existing paths
      if the `show_rel` debug flag is set.
    
    The debug printouts are sent back to the client as debug messages
    (`DEBUG2`), allowing developers to quickly experiment and interactively
    see what effect different statements has with the respect to the paths
    generated.
    
    In addition, the handling of `debug_optimizer_flag` was not correct and
    is fixed here.
    
    * If no `=` was provided to `show_upper`, it triggered a segmentation
      fault.
    
    * If the flag was reset, the internal data structure was not updated.
    
    * If just one flag was updated in a `SET` command, the other flag was
      kept intact.

---
+++ tsl/src/debug.h
@@ -0,0 +1,21 @@
+/*
+ * This file and its contents are licensed under the Apache License 2.0.
+ * Please see the included NOTICE for copyright information and
+ * LICENSE-APACHE for a copy of the license.
+ */
+#ifndef TIMESCALEDB_DEBUG_H
+#define TIMESCALEDB_DEBUG_H
+
+#include <nodes/relation.h>
+#include <lib/stringinfo.h>
+#include <utils/guc.h>
+
+#ifdef TS_DEBUG
+extern const char *upperrel_stage_name[UPPERREL_FINAL + 1];
+extern void tsl_debug_append_rel(StringInfo buf, PlannerInfo *root, RelOptInfo *rel);
+extern void tsl_debug_append_path(StringInfo buf, PlannerInfo *root, Path *path, int indent);
+extern void tsl_debug_append_pathlist(StringInfo buf, PlannerInfo *root, List *pathlist,
+									  int indent);
+#endif
+
+#endif /* TIMESCALEDB_DEBUG_H */
